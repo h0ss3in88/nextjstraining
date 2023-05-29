f470c350d4dae6df11539cfba50b97d90832f6c0
h0ss3in88 <husseinDeveloperx@protonmail.com>
2023-05-30 02:27:16 +0430

    express installed & server + little bit html file established.

---
+++ src/server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const logger =require("morgan");
+const path = require("path");
+const {createServer} = require("http");
+let app = express();
+app.use(logger("dev"));
+app.get("/", (req,res) => {
+    return res.sendFile(path.resolve(__dirname, "../../","index.html"));
+});
+let server = createServer(app);
+server.listen(3200, () => {
+    console.log(`server is running at ${server.address().address}:${server.address().port}`);
+})
