81b0c2eec54c3ac94cc7b07ce43d65166fbe7fd5
h0ss3in88 <husseinDeveloperx@protonmail.com>
2023-05-30 03:22:07 +0430

    add react to the project as well as babel compiler.

--- src/server/app.js
+++ src/server/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const {createServer} = require("http");
 let app = express();
 app.use(logger("dev"));
+app.use("/client",express.static(path.resolve(__dirname,"../","client")));
 app.get("/", (req,res) => {
     return res.sendFile(path.resolve(__dirname, "../../","index.html"));
 });
