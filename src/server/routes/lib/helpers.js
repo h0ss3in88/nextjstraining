77d6cc2f21124b29e2c58a4a49b5570454c07b9c
h0ss3in88 <husseinDeveloperx@protonmail.com>
2023-06-09 04:07:44 +0430

    complete route section for products feature of application successfully.

--- src/server/routes/lib/helpers.js
+++ src/server/routes/lib/helpers.js
@@ -1,3 +1,4 @@
+const {body, validationResult} = require("express-validator");
 module.exports = {
     "invalidIdMiddlware" : function (req,res,next) {
         if(req.productId !== null && req.productId !== undefined) {
@@ -5,5 +6,24 @@ module.exports = {
         }else {
             return next(new Error("invalid input parameters"));
         }
-    }
+    },
+    "checkProductUpdate" : [
+        body("productName").notEmpty().escape().withMessage("name should not be empty"),
+        body("productId").notEmpty().isUUID("4").escape().withMessage("should not be empty"),
+        body("price").notEmpty().isNumeric().isCurrency().escape().withMessage("price should not be empty | should be numeric"),
+        body("count").notEmpty().isNumeric().escape().withMessage("invalid value for count property"),
+        body("description").isLength({ min: 0 ,max: 550}).escape(),
+        function(req,res,next) {
+            let errorResult = validationResult(req);
+            if(!errorResult.isEmpty()) {
+                let errors = errorResult.array({onlyFirstError: false }).map((error) => {
+                    return { "message" : error.msg, "path": error.path };
+                })
+                let errorMessage = `invalid inputs ${errors}`;
+                return next(new Error(errorMessage));
+            }
+            next();
+        }
+    ]
+    
 }
--- src/server/routes/lib/products.js
+++ src/server/routes/lib/products.js
@@ -1,12 +1,13 @@
 const {Router} = require("express");
-const {invalidIdMiddlware} = require("./helpers");
+const {invalidIdMiddlware,checkProductUpdate} = require("./helpers");
 const httpStatus = require("http-status");
+const {v4 : uuid} = require("uuid");
 let productRouter = Router();
 productRouter.param("id", (req,res,next,id) => {
     try {
         let productId = parseInt(id, 10);
         if(productId !== null && productId !== undefined) {
-            req.productId = productId;
+            req.productId = parseInt(productId, 10);
             return next();
         }else {
             return next();
@@ -16,18 +17,52 @@ productRouter.param("id", (req,res,next,id) => {
     }
 });
 productRouter.route("/products/:id?")
-    .get((req,res,next) => {
-        if(req.productId !== undefined && req.productId !== null) {
-            return res.status(httpStatus.OK).json({ product: []});
-        }else {
-            return res.status(httpStatus.OK).json({ product: []});
+    .get(async (req,res,next) => {
+        try {
+            if(req.productId !== undefined && req.productId !== null) {
+                let product = await req.repos.productRepo.getProductById(req.productId);
+                return res.status(httpStatus.OK).json({ product});
+            }else {
+                let products = await req.repos.productRepo.getProducts();
+                return res.status(httpStatus.OK).json({ products });
+            }
+        } catch (error) {
+            return next(new Error(error.message));
+        }    
+    }).post(async (req,res,next) => {
+        try {
+
+            let productId = uuid();
+
+            let {id,name,description,price,count} = req.body.product;
+            //TODO check inputs 
+            //TODO add uuid as productId
+            let item = { id,name,productId, description,price,count};
+            //TODO save product
+            let result = await req.repos.productRepo.createProduct(item);
+            return result > 0 ? res.status(httpStatus.CREATED).json({ message : "product saved successfully", success: true , product : item}) : 
+                next(new Error("unable to save product into database. try again!"));
+        } catch (error) {
+            return next(new Error(error.message));
         }
-    }).post((req,res,next) => {
-
-    }).put([invalidIdMiddlware], (req,res,next) => {
+    }).put([invalidIdMiddlware,checkProductUpdate], async (req,res,next) => {
+        //TODO! update specific product by ID 
+        try {
+            let productId = req.productId;
+            let editedProduct = {id: productId, productId: req.body.productId, name: req.body.productName, description: req.body.description, price: req.body.price, count: req.body.count };
+            let result = await req.repos.productRepo.updateProduct(productId, editedProduct);
+            return result > 0 && result === productId ? res.status(httpStatus.OK).json({ message : `product by product id of ${productId} edited successfully`, success : true , updatedId : result }) : next(new Error("unable to update product"));
+        } catch (error) {
+            return next(new Error(error.message));
+        }
+    }).delete([invalidIdMiddlware], async (req,res,next) => {
         //TODO! delete specific product by ID 
-    }).delete([invalidIdMiddlware], (req,res,next) => {
-        //TODO! delete specific product by ID 
-
+        try {
+            let productId = req.productId;
+            let result = await req.repos.productRepo.deleteProduct(productId);
+            return result > 0 && result == productId ? res.status(httpStatus.OK).json({ success : true , message : `product by id ${productId} deleted successfully.`, deletedId: result}) : res.status(httpStatus.INTERNAL_SERVER_ERROR).json({error: new Error(`unable to delete product by id = ${productId} try again later ...`)});
+        }catch(error) {
+            return next(new Error(error.message));
+        }
     });
 module.exports = Object.assign({}, {productRouter});
