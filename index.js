373d61470abdcb9c819a0e8fe2c8c3cc4ab04f98
h0ss3in88 <husseinDeveloperx@protonmail.com>
2023-06-09 04:00:56 +0430

    refactor starter file of app & add not found and error handling middlewares.

---
+++ index.js
@@ -0,0 +1,39 @@
+const { createServer } = require("http");
+const { initApp } = require("./src/server/app");
+const { initializeRepo } = require("./src/server/db");
+const { saveProducts } = require("./src/server/helpers");
+require('dotenv').config();
+(function() { initializeRepo().then(async ({ productRepo }) => {
+    console.log("repositories initialized successfully");
+    let productNumber = parseInt(process.env.PRODUCT_NUMBER, 10);
+    await productRepo.createTable();
+    saveProducts({productRepo, productNumber }).then((result) => {
+        console.log(typeof(result.length),typeof(productNumber));
+        if(parseInt(result.length,10) === parseInt(productNumber, 10)) {
+            console.log("seed database successfully");
+            let repos = {productRepo};
+            initApp({ repos }).then(app => {
+                let server = createServer(app);
+                server.listen(3200, () => {
+                    console.log(`server is running at ${server.address().address}:${server.address().port}`);
+                });
+            }).catch(error => {
+                console.log(error);
+                process.exit(0);
+            });
+        }else {
+            console.log(error);
+
+            process.exit(0);
+        }
+    }).catch(error => {
+        console.log(error);
+
+            process.exit(0);
+    });
+
+}).catch(error => {
+    console.log(error);
+    process.exit(0);
+});
+})();
--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const logger =require("morgan");
 const path = require("path");
-const {createServer} = require("http");
-let app = express();
-app.use(logger("dev"));
-app.use("/client",express.static(path.resolve(__dirname,"../","client")));
-app.get("/", (req,res) => {
-    return res.sendFile(path.resolve(__dirname, "../../","index.html"));
-});
-let server = createServer(app);
-server.listen(3200, () => {
-    console.log(`server is running at ${server.address().address}:${server.address().port}`);
-})
+const status = require("http-status");
+const {routesSetup} = require("./routes");
+
+const initApp = function({repos}) {
+    return new Promise((resolve,reject) => {
+        let app = express();
+        app.use(logger("dev"));
+        app.use("/client",express.static(path.resolve(__dirname,"../","client")));
+        app.use(express.json());
+        app.get("/", (req,res) => {
+            // return res.sendFile(path.resolve(__dirname, "../../","index.html"));
+            return res.sendFile(path.resolve(__dirname,"../","client","app","home.html"));
+        });
+        app.use((req,res,next) => {
+           req.repos = repos;
+           return next();
+        });
+        routesSetup({app});
+        app.use((req,res,next) => {
+            let err = new Error("Not Found!");
+            return next(err);
+        });
+        app.use((err,req,res,next) => {
+            if(err.message === "Not Found!") {
+                return res.status(status.NOT_FOUND).json({message: err.message , error: err});
+            }else {
+                return res.status(status.INTERNAL_SERVER_ERROR).json({error: err , message: err.message});
+            }
+        });
+        return resolve(app);
+    });
+}
+module.exports = Object.assign({}, {initApp});
+
+
